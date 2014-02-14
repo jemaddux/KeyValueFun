63673d1a1fd12eb981d6efc9df5d06e83c515d2b
John Maddux <jemaddux@gmail.com>
2014-02-13 20:32:38 -0500

    added a user

---
+++ app/models/User.rb
@@ -0,0 +1,27 @@
+class User
+  PROPERTIES = [:id, :name, :email]
+  PROPERTIES.each { |prop|
+    attr_accessor prop
+  }
+
+  def initialize(attributes = {})
+    attributes.each { |key, value|
+      self.send("#{key}=", value) if PROPERTIES.member? key
+    }
+  end
+
+  def initWithCoder(decoder)
+    self.init
+    PROPERTIES.each { |prop|
+      value = decoder.decodeObjectForKey(prop.to_s)
+      self.send((prop.to_s + "=").to_s, value) if value
+    }
+    self
+  end
+
+  def encodeWithCoder(encoder)
+    PROPERTIES.each { |prop|
+      encoder.encodeObject(self.send(prop), forKey: prop.to_s)
+    }
+end
+
